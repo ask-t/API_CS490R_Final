e320e5b11081419005e3ff80b95c7c5b4a3a651f
ask-t <asktakahashi@go.byuh.edu>
2024-05-23 14:58:47 -1000

    isntall moongose

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+mongoose.connect(process.env.MONGO_URI,)
+.then(() => {
+  console.log("Connected to MongoDB...");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Linstening on port ${port}`));
+})
+.catch(err => console.error("Could not connect to MongoDB...", err));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Linstening on port ${port}`));
