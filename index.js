87cff70277d64268ac16ad24c3c2b923b598f771
ask-t <asktakahashi@go.byuh.edu>
2024-05-23 15:10:04 -1000

    add models, and routes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/products", productRoute);
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
