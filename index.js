3911e896793a0bc1dd8ca431ab656b9410e1cf3c
ask-t <asktakahashi@go.byuh.edu>
2024-05-23 15:15:26 -1000

    install dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
