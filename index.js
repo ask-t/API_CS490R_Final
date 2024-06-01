668903e6cca0affdc285612d7907d2e6800e229b
ask-t <asktakahashi@go.byuh.edu>
2024-05-31 22:07:15 -1000

    update the program for docker

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-mongoose.connect(process.env.MONGO_URI,)
-.then(() => {
-  console.log("Connected to MongoDB...");
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Linstening on port ${port}`));
-})
-.catch(err => console.error("Could not connect to MongoDB...", err));
+// mongoose.connect(process.env.MONGO_URI,)
+// .then(() => {
+//   console.log("Connected to MongoDB...");
+//   const port = process.env.PORT || 3000;
+//   app.listen(port, () => console.log(`Linstening on port ${port}`));
+// })
+// .catch(err => console.error("Could not connect to MongoDB...", err));
 
+mongoose
+  .connect("mongodb://mongo:27017/quote")
+  .then(() => {
+    console.log("Connected to MongoDB...");
+    port =8080
+    app.listen(port, () => console.log(`Linstening on port ${port}`));
+  })
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
