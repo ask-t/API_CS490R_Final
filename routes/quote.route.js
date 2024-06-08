ab29effa4e81ae29e53936cb869005f10db5369d
ask-t <asktakahashi@go.byuh.edu>
2024-06-08 01:40:52 -1000

    add authenticate

--- routes/quote.route.js
+++ routes/quote.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { getQuotes, getQuote, createQuote, updateQuote, deleteQuote, test } = require("../controllers/quote.controller.js");
+const passport = require("passport");
 
 router.get("/", getQuotes);
 router.get("/:id", getQuote);
-router.post("/", createQuote);
-router.put("/:id", updateQuote);
-router.delete("/:id", deleteQuote);
+router.post("/", passport.authenticate("jwt", { session: false }), createQuote);
+router.put("/:id", passport.authenticate('jwt', { session: false }),updateQuote);
+router.delete("/:id",passport.authenticate('jwt', { session: false }), deleteQuote);
 router.get("/test/test", test);
 
 module.exports = router;
